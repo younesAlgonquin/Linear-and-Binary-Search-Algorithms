530f3d5f402a20e766414ba1898c8b92f03e326e
Younes <77597800+younesAlgonquin@users.noreply.github.com>
2021-08-26 10:57:35 -0400

    Add files via upload

---
+++ BinaryLinearSearch.java
@@ -0,0 +1,286 @@
+import java.security.SecureRandom;
+import java.util.Arrays;
+
+/**
+ * CET - CS Academic Level 3
+ * Declaration: I declare that this is my own original work and is free from Plagiarism
+ * This class implements a method that generates an array of random integers and sorts it.
+ * then it declares methods for iterative & recursive binary & linear search of an integer within this array.
+ * Student Name: Younes Boutaleb
+ * Student Number: 041019068 
+ * Section #: 303
+ * Course: CST8130 - Data Structures
+ * Professor: James Mwangi PhD.
+ * @author Younes Boutaleb.
+ * @version 1.0
+ * 
+ */
+public class BinaryLinearSearch {
+	
+	/**This is the array that will be used to perform the search*/
+	private int[] randomArr;
+	/**this the index of the user's integer if it exists within the array*/
+	private int location;
+	
+	/**
+	 * This is a parameterized constructor that initializes the randomArr array
+	 * @param size this is the size of the randomArr array
+	 */
+	public BinaryLinearSearch(int size){
+		
+		randomArr= new int[size];
+	}//end the parameterized constructor 
+	
+	/**
+	 * This method generates an array of random integers sort it then prints it
+	 * @return Returns a sorted array
+	 */
+	public int[] generateRandomInts() {
+		
+		//SecureRandom object which generates random integers 
+		SecureRandom random = new SecureRandom();
+		//this the generated integer
+		int number;
+		//generates random integers and stores them in randomArr
+		for(int i=0; i<randomArr.length; i++) {
+			number= random.nextInt(89) + 11;
+			randomArr[i]=number;
+		}//end for
+		//sorts the elements of randomArr
+		Arrays.sort(randomArr);
+		//prints the elements of randomArr
+		System.out.print(Arrays.toString(randomArr));
+		
+		return randomArr;
+	}//end generateRandomInts
+	
+	/**
+	 * This method performs iterative binary search for a given integer within a given array
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param key user's integer
+	 * @return this method returns the index of key integer if it exists within the array otherwise returns -`1
+	 */
+	public int iterativeBinarySearch(int[] randomArr, int key) {
+		
+		location=-1;
+		//The array's boundaries
+		int maxIndex=randomArr.length -1;
+		int minIndex=0;
+		//Loops until it finds the key or when no remaining values to check
+		while(maxIndex>=minIndex && location==-1) {
+		
+			//Displays the remaining possibles matches to the key integer after each iteration
+			remainingElements(minIndex, maxIndex);
+			//this is the index of the middle element of the remaining elements
+			int index = (maxIndex + minIndex + 1)/2;
+			//Compares the key integer to the middle element. if it matches it assigns index to location
+			if(key==randomArr[index]) {
+				location=index;
+				System.out.printf("\nnumber %d found at index %d: Iterative Binary Search", key, location);
+			//if not it identifies the remaining possible matches
+			}else if(key>randomArr[index]) {
+				minIndex=index+1;
+			}else {
+				maxIndex=index-1;
+			}
+			}//end while
+		
+		//if not match found then the method returns -1
+		if(location==-1) {
+			System.out.printf("\nnumber %d was not found: Iterative Binary Search", key);
+		}		
+		return location;
+	}//end iterativeBinarySearch
+	
+	/**
+	 * This method calculates and displays the processing time of the iterative Binary Search
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param key user's integer
+	 */
+	public void iterativeBinarySearchTiming (int[] randomArr, int key) {
+		
+		//this is the time in the beginning of the iterative Binary Search in Nanosecond
+		long timeNano= System.nanoTime();
+		//this is the time in the beginning of the iterative Binary Search in millisecond
+		long timeMillli= System.currentTimeMillis();
+		//Performs the iterative Binary Search
+		iterativeBinarySearch(randomArr, key);
+		//this is the duration of the iterative Binary Search in Nanosecond
+		timeNano= System.nanoTime()-timeNano;
+		//this is the duration of the iterative Binary Search in millisecond
+		timeMillli= System.currentTimeMillis()- timeMillli;
+		
+		System.out.printf("\nTime taken in nanoseconds: %d", timeNano);
+		System.out.printf("\nTime taken in milliseconds: %d", timeMillli);
+	}//end iterativeBinarySearchTiming 
+	
+	/**
+	 * This method performs recursive binary search for a given integer within a given array
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param minIndex this is the lowest index of the remaining elements
+	 * @param maxIndex this is the highest index of the remaining elements
+	 * @param key user's integer
+	 * @return this method returns the index of key integer if it exists within the array otherwise returns -`1
+	 */
+	public int recursiveBinarySearch (int[] randomArr,int minIndex, int maxIndex, int key) {
+		
+		location=-1;
+		//Displays the remaining possibles matches to the key integer after each method call
+		remainingElements(minIndex, maxIndex);
+		//returns -1 when no match is found
+		if((maxIndex<minIndex) && (location==-1)) {
+			System.out.printf("\nnumber %d was not found: Recursive Binary Search", key);
+		//Compares the key integer to the middle element. if it matches it assigns index to location
+		}else {
+			int index = (maxIndex + minIndex + 1)/2;	
+			if(key==randomArr[index]) {
+				location=index;
+				System.out.printf("\nnumber %d found at index %d: Recursive Binary Search", key, location);
+			//if not it identifies the remaining possible matches then recalls the recursiveBinarySearch method
+			}else { 
+					if(key>randomArr[index]) {
+					minIndex=index+1;	
+					}else {
+					maxIndex=index-1;
+					}//end else
+				recursiveBinarySearch (randomArr,minIndex, maxIndex, key);
+					}//end else
+				}//end else
+		
+		return location;
+	}//end recursiveBinarySearch
+	
+	/**
+	 * This method calculates and displays the processing time of the recursive Binary Search
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param minIndex this is the lowest index of the remaining elements
+	 * @param maxIndex this is the highest index of the remaining elements
+	 * @param key user's integer
+	 */
+	public void recursiveBinarySearchTiming (int[] randomArr,int minIndex, int maxIndex, int key) {
+		
+		//this is the time in the beginning of the iterative Binary Search in Nanosecond
+		long timeNano= System.nanoTime();
+		//this is the time in the beginning of the iterative Binary Search in millisecond
+		long timeMillli= System.currentTimeMillis();
+		//Performs the recursive Binary Search
+		recursiveBinarySearch (randomArr, minIndex, maxIndex, key);
+		
+		//this is the duration of the iterative Binary Search in Nanosecond
+		timeNano= System.nanoTime()-timeNano;
+		//this is the duration of the iterative Binary Search in millisecond
+		timeMillli= System.currentTimeMillis()- timeMillli;
+		System.out.printf("\nTime taken in nanoseconds: %d", timeNano);
+		System.out.printf("\nTime taken in milliseconds: %d", timeMillli);
+	}//end recursiveBinarySearchTiming 
+	
+	/**
+	 * This method performs iterative Linear search for a given integer within a given array
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param key user's integer
+	 * @return this method returns the index of key integer if it exists within the array otherwise returns -`1
+	 */
+	public int iterativeLinearSearch(int[] randomArr, int key) {
+			
+		location=-1;
+		//iterates within the array and looks for the key integer. if it finds a match it breaks out of the foor loop
+		for(int i=0; i<randomArr.length; i++) {
+			if(key==randomArr[i]) {
+				location=i;
+				System.out.printf("\nnumber %d found at index %d: Iterative Linear Search", key, location);
+				break;
+			}//end if
+		}//end for
+		
+		//if no match it returns -1
+		if(location==-1) {
+			System.out.printf("\nnumber %d was not found: Iterative Linear Search", key);
+		}
+		return location;
+	}//end iterativeLinearSearch
+	
+	/**
+	 * This method calculates and displays the processing time of the iterative Linear Search
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param key user's integer
+     */
+	public void iterativeLinearSearchTiming (int[] randomArr, int key) {
+		
+		//this is the time in the beginning of the iterative Binary Search in Nanosecond
+		long timeNano= System.nanoTime();
+		//this is the time in the beginning of the iterative Binary Search in millisecond
+		long timeMillli= System.currentTimeMillis();
+		//Performs the iterative Linear Search
+		iterativeLinearSearch(randomArr, key);
+		//this is the duration of the iterative Binary Search in Nanosecond
+		timeNano= System.nanoTime()-timeNano;
+		//this is the duration of the iterative Binary Search in millisecond
+		timeMillli= System.currentTimeMillis()- timeMillli;
+		System.out.printf("\nTime taken in nanoseconds: %d", timeNano);
+		System.out.printf("\nTime taken in milliseconds: %d", timeMillli);
+
+	}//end iterativeLinearSearchTiming 
+	
+	/**
+	 * This method performs recursive Linear Search for a given integer within a given array
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param size is randomArr size
+	 * @param key user's integer
+	 * @return this method returns the index of key integer if it exists within the array otherwise returns -`1
+	 */
+	public int recursiveLinearSearch (int[] randomArr, int size,int key) {
+				
+		location=-1;
+		//if no match in the entire array it returns -1
+		if(size==0 && location==-1) {
+			System.out.printf("\nnumber %d was not found: Recursive Linear Search", key);
+		/*The method compares an array element to the key integer if they match it returns the element index
+		if not it recalls itself on the following element */
+		}else {
+			if(key == randomArr[randomArr.length-size]) {
+				location=randomArr.length-size;
+				System.out.printf("\nnumber %d found at index %d: Recursive Linear Search", key, location);
+			}else
+				recursiveLinearSearch (randomArr, size-1,key);
+		}//end else
+				
+		return location;
+	}//end recursiveLinearSearch
+	
+	/**
+	 * This method calculates and displays the processing time of the recursive Linear Search
+	 * @param randomArr is the sorted array where we want to search for user's integer
+	 * @param size is randomArr size
+	 * @param key user's integer
+	 */
+	public void recursiveLinearSearchTiming (int[] randomArr, int size,int key) {
+		
+		//this is the time in the beginning of the iterative Binary Search in Nanosecond
+		long timeNano= System.nanoTime();
+		//this is the time in the beginning of the iterative Binary Search in millisecond
+		long timeMillli= System.currentTimeMillis();
+		//Performs the iterative Linear Search
+		recursiveLinearSearch (randomArr, size,key);
+		//this is the duration of the iterative Binary Search in Nanosecond
+		timeNano= System.nanoTime()-timeNano;
+		//this is the duration of the iterative Binary Search in millisecond
+		timeMillli= System.currentTimeMillis()- timeMillli;
+		System.out.printf("\nTime taken in nanoseconds: %d", timeNano);
+		System.out.printf("\nTime taken in milliseconds: %d", timeMillli);
+		
+	}//end recursiveLinearSearchTiming
+		
+	/**
+	 * 
+	 * @param minIndex the lowest index of the range of randomArr elements to be printed
+	 * @param maxIndex the highest index of the range of randomArr elements to be printed
+	 */
+	public void remainingElements(int minIndex, int maxIndex) {
+		//prints specified range of elements
+		System.out.println();
+		for(int i=minIndex; i<=maxIndex; i++) {
+			System.out.print(randomArr[i] + " ");
+		}//end for
+	}//end remainingElements
+	
+}//end class
---
+++ Lab2BinLinSearchTest.java
@@ -0,0 +1,117 @@
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * CET - CS Academic Level 3
+ * 
+ * Declaration: I declare that this is my own original work and is free from Plagiarism
+ * This class tests the program by displaying a menu of options and prompts users to choose one of them.
+ * then it displays an array of integers and performs binary and iterative search
+ * Student Name: Younes Boutaleb
+ * Student Number: 041019068 
+ * Section #: 303
+ * Course: CST8130 - Data Structures
+ * Professor: James Mwangi PhD.
+ * @author Younes Boutaleb.
+ * @version 1.0
+ * 
+ */
+public class Lab2BinLinSearchTest {
+
+	/**
+	 * This is the method main where we test our program
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		//Scanner object used to get the user's inputs
+		Scanner input = new Scanner(System.in).useDelimiter("\r\n");
+		//BinaryLinearSearch object used to declare an array and call search methods
+		BinaryLinearSearch demo = new BinaryLinearSearch(30);
+		//this array will store the element of the generated array
+		int[] randomArr = null;
+		//This is the menu option that will be entered by users
+		int option =0;
+		//The while loop display the menu and executes the chosen option in each iteration until the Exit option is chosen 
+		while(option !=4) {
+			
+			//this formats and prints the options menu
+			String formatString = "\n\nSelect your option in the menu:\n";
+			formatString += "%d. Initialize and populate an array of 30 random integers.\n";
+			formatString += "%d. Perform recursive binary and linear search.\n";
+			formatString += "%d. Perform iterative binary and linear search.\n";
+			formatString += "%d. Exit.\n";
+			System.out.print(String.format(formatString, 1, 2, 3, 4));
+		
+			//checks for integer input
+			try {
+				option = input.nextInt();
+				
+				//This is the switch statement  that executes the user's choice
+				switch(option) {
+				//in this case it generates an array of random integers, sorts it, and prints it
+				case 1: 
+					randomArr= (demo.generateRandomInts()).clone();
+					break;
+			
+				//in this case the program prompts users to enter the value to search then displays the recursive binary & linear search results and timing
+				case 2:
+					//checks for integer input
+					try {
+						System.out.print("Please enter an integer value to search: ");
+						int key = input.nextInt();
+						//calls the recursive search methods
+						demo.recursiveBinarySearchTiming(randomArr, 0, randomArr.length-1, key);
+						demo.recursiveLinearSearchTiming(randomArr, randomArr.length, key);
+					//it outputs an error message in case of non-integer input
+					}catch (InputMismatchException ex) {
+						System.out.printf("*****Input Mismatch Exception*****", ex);
+						input.nextLine();
+					}//end try & catch
+					//ends the switch statement 
+					break;
+			
+				//in this case the program prompts users to enter the value to search then displays the iterative binary & linear search results and timing
+				case 3:
+					//checks for integer input
+					try {
+						System.out.print("Please enter an integer value to search: ");
+						int key = input.nextInt();
+						//calls the recursive search methods
+						demo.iterativeBinarySearchTiming(randomArr, key);
+						demo.iterativeLinearSearchTiming(randomArr, key);
+					//it outputs an error message in case of non-integer input
+					}catch (InputMismatchException ex) {
+						System.out.printf("*****Input Mismatch Exception*****", ex);
+						input.nextLine();
+					}//end try & catch
+					//ends the switch statement 
+					break;
+			
+				//This option shutdown the program
+				case 4:
+					System.out.println("exiting...");
+					//ends the switch statement 
+					break;
+			
+				//it outputs an error message if the option input is not 1, 2, 3 or 4
+				default:
+					System.out.println("Please choose the option 1 to 4.");
+					//ends the switch statement 
+					break;
+	
+				}//end switch
+			//it outputs an error message in case of non-integer input
+			}catch (InputMismatchException ex) {
+			System.out.printf("*****Input Mismatch Exception*****", ex);
+			input.nextLine();
+			}//end try & catch
+		}//end while
+		
+		//closes the Scanner object
+		input.close();
+		
+	}//end main	
+
+}//end class
